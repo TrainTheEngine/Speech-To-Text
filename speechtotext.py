4780319f13222b8960294f712e9f2bb74b0c8bf3
TrainTheEngine <tongastrainengine@gmail.com>
2023-07-14 09:30:54 +0100

    Added Flask templates.
    Added unfinished html templates and almost finished code.
    Templates stored in subdirectory, currently hard-coded.
    Design themes currently inconsistent, will need reworking.

--- speechtotext.py
+++ speechtotext.py
@@ -1,11 +1,15 @@
+from flask import Flask, render_template, request
 import whisper
-from flask import Flask
-app = Flask(__name__)
-@app.route("/")
-def speechtotext():
+app = Flask(__name__, template_folder="C:/Users/Tom/Documents/Cloudera/Templates")
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "GET":
+        return render_template("initemp.html")
+    fyl = request.form.get("fyl")
+    loca = request.form.get("loca")
     model = whisper.load_model("base")
-    fl = request.args["Enter File Location: "]
-    result = model.transcribe(fl)
-    print(result["text"])
-if __name__ == "__main__":
-    app.run()
+    result = model.transcribe(fyl, fp16=False)
+    file = open(loca, "w")
+    file.write(result["text"])
+    return render_template("postemp.html")
+app.run()
