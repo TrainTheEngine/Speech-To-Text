bc685c62e5dc96c05c29500daf7c3d325f836fe4
TrainTheEngine <tongastrainengine@gmail.com>
2023-07-14 09:28:51 +0100

    Added flask page and user input.

--- speechtotext.py
+++ speechtotext.py
@@ -1,5 +1,11 @@
 import whisper
-
-model = whisper.load_model("base")
-result = model.transcribe("harvard.wav")
-print(result["text"])
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def speechtotext():
+    model = whisper.load_model("base")
+    fl = request.args["Enter File Location: "]
+    result = model.transcribe(fl)
+    print(result["text"])
+if __name__ == "__main__":
+    app.run()
